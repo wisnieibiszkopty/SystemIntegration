d18488f03f9b8d52216dd57745c0a0486eefd879
Kamil Wodowski <s97766@pollub.edu.pl>
2024-04-26 12:19:36 +0200

    initial commit

---
+++ backend/src/main/java/com/project/steamtwitchintegration/SteamTwitchIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.project.steamtwitchintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamTwitchIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamTwitchIntegrationApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/steamtwitchintegration/SteamTwitchIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.steamtwitchintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamTwitchIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
